b445b04e17275fef75633e079d1c56d2bd62c132
Lochinbek <lochinbek.b@jdu.uz>
2023-04-10 15:04:52 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include "partials/header.html";
+?>
+
+<h1>Home Page</h1>
+
+<?php
+include "partials/footer.html";
+?>
+    
---
+++ pages/about.php
@@ -0,0 +1,10 @@
+<?php
+include "../partials/header.html";
+?>
+
+<h1>About Page</h1>
+
+<?php
+include "../partials/footer.html";
+?>
+    
